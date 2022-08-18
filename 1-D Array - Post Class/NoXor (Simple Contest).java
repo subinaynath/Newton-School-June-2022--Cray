463b273ce621512333077e09d43235c92bfd18bf
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-08-18 13:21:05 +0530

    Create NoXor (Simple Contest).java

---
+++ 1-D Array - Post Class/NoXor (Simple Contest).java
@@ -0,0 +1,23 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        long sum=0;
+		long a[]=new long[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextLong();
+		}
+
+		
+		for(int i=1;i<=n;i++){
+			sum+=((n-i)^a[i-1]);
+		}
+
+		System.out.print(sum);
+	}
+}
