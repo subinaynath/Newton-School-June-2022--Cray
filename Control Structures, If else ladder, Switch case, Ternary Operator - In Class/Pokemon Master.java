336b06dfdf0c80840f3da0ba90479685176b01fb
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-08-18 11:54:20 +0530

    Create Pokemon Master.java

---
+++ Control Structures, If else ladder, Switch case, Ternary Operator - In Class/Pokemon Master.java
@@ -0,0 +1,9 @@
+function PokemonMaster(a,b) {
+    // write code here
+    // do not console.log the answer
+    // return the output using return keyword
+if (a>=b){
+    return 1;
+}else return 0
+
+}
