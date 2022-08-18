5a9c1fd1a060f71c90dba688a4f6fe47d8be756a
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-08-18 13:17:51 +0530

    Create Rectangle.java

---
+++ Loops - In Class/Rectangle.java
@@ -0,0 +1,24 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+                  I'm    Scanner sc =new Scanner(System.in);
+                        int n = sc.nextInt();
+                        int m = sc.nextInt();
+       for(int i=0; i<n; i++) {
+           for(int j=0; j<m; j++) {
+               if(i == 0 || i == n-1 || j == 0 || j == m-1) {
+                   System.out.print("*");
+               } else {
+                   System.out.print(" ");
+               }
+           }
+           System.out.println();
+       }
+
+    }
+}
