3553bd3ff859673d9888d2da9c92d6c46cb35da1
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-08-18 13:16:15 +0530

    Create Ice cream.java

---
+++ Loops - In Class/Ice cream.java
@@ -0,0 +1,11 @@
+static int Icecreams (int N, int D){
+//Enter your code here
+int eat=N;
+int left = 0;
+for ( int i=1;i<=D;i++){
+    eat = (N/2)+(N%2);
+    N =eat * 3;
+    
+    
+}return N;
+}
