c255b224bb8abf94aba63b7a7f870cb074f324fd
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-08-18 13:19:27 +0530

    Create Pattern Printing.java

---
+++ Loops - Post Class/Pattern Printing.java
@@ -0,0 +1,11 @@
+static void pattern(int n){
+//enter your code here
+int i,j;
+   for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+                System.out.print(j + " ");
+
+            System.out.println("");
+        }
+}
