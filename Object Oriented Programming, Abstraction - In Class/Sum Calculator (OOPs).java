5380707bfb5e2747f0924a8695f70813a22d2cbd
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-09-02 17:53:20 +0530

    Create Sum Calculator (OOPs).java

---
+++ Object Oriented Programming, Abstraction - In Class/Sum Calculator (OOPs).java
@@ -0,0 +1,20 @@
+// create class here and its methods as well
+class SumCalculator{
+    int num1;
+    int num2;
+    
+    SumCalculator(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public int sum() {
+        return (this.num1 + this.num2);
+    }
+    public  int sum2(int a, int b) {
+        return (a+b);
+    }
+    public int fromObject (SumCalculator obj1, SumCalculator obj2){
+        return obj1.sum() + obj2.sum();
+    }
+
+}
