a87ac3d2f60b842752f47806d7b70e903ad58b97
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-09-09 13:19:47 +0530

    Create implement & add element in linklist.java

---
+++ Linklist/implement & add element in linklist.java
@@ -0,0 +1,72 @@
+class Main{
+    public static void main(String[] args) {
+        Linklist ll = new Linklist();
+        int k=4;
+        ll.display();
+        ll.add(0);
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        ll.add(8);
+        ll.display();
+        System.out.println();
+        ll.kth(ll.head,k);
+        ll.display();
+
+
+
+    }
+}
+
+class Linklist{
+   public class Node{
+       int data;
+       Node next;
+       Node(int data){
+           this.data=data;
+           this.next=null;
+       }
+   }
+   Node head;
+
+   // too add element in linklist
+   public Node add(int x){
+       Node newNode= new Node(x);
+       if(this.head == null){
+           this.head= newNode;
+           return this.head;
+       }
+       Node tr=this.head;
+       while(tr.next != null){
+           tr=tr.next;
+       }
+       tr.next=newNode;
+       return  this.head;
+   }
+
+   public void display(){
+       Node tr= this.head;
+       if(tr == null){
+           System.out.println("null");
+           return;
+       }
+       while(tr != null){
+           System.out.print(tr.data+" ");
+           tr=tr.next;
+       }
+   }
+    // make till kth node
+    public Node kth(Node head, int k){
+       int count=1;
+       Node tr=head;
+       while (count !=k){
+           tr=tr.next;
+           count++;
+       }
+       tr.next= null;
+       return  head;
+    }
+
+}
