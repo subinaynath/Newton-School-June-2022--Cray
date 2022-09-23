f219c0c4d8f64df5b64682e9e6025bea91229825
Subinay Nath <106378294+subinaynath@users.noreply.github.com>
2022-09-23 17:49:27 +0530

    Update Print a spiral matrix in anticlockwise.java

--- Interview Help Question/Print a spiral matrix in anticlockwise.java
+++ Interview Help Question/Print a spiral matrix in anticlockwise.java
@@ -7,35 +7,37 @@ import java.util.*;
   13 14 15 16
 */
 class Interview {
-    public static void antiCloclSpiral(int arr[][]){
-        int i, k = 0, l = 0, m=arr.length-1, n=arr[0].length-1;
+    public static void spiral(int arr[][]){
+        int i, k = 0, l = 0, m=arr.length, n=arr[0].length;
         int lefttopr=m*n;
         while(lefttopr >0){
-            for (i = k; i <= n; ++i) {
+            for (i = k; i < n; ++i) {
                 System.out.print(arr[i][l] + " ");
                 lefttopr--;
             }
             l++;
-            for (i = l; i <=n; ++i) {
-                System.out.print(arr[m][i] + " ");
+            for (i = l; i < n; ++i) {
+                System.out.print(arr[m - 1][i] + " ");
                 lefttopr--;
             }
             m--;
-            for (i = m; i >= k; --i) {
-                System.out.print(arr[i][n] + " ");
+            for (i = m - 1; i >= k; --i) {
+                System.out.print(arr[i][n - 1] + " ");
                 lefttopr--;
             }
             n--;
-            for (i = n; i >= l; --i) {
+            for (i = n - 1; i >= l; --i) {
                 System.out.print(arr[k][i] + " ");
                 lefttopr--;
             }
             k++;
+
+
         }
     }
     public static void main(String[] args) {
         int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 
-        antiCloclSpiral(arr);
+        spiral(arr);
     }
 }
